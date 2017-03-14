b76d4f875cd77aa358c6b7700f5fefd2dc60a47d
lixiaohao <lixiaohaono1@163.com>
2017-03-14 14:33:34 +0800

    image

---
+++ js/index.js
@@ -0,0 +1,54 @@
+$(function () {
+    getAllResources();
+});
+
+function getAllResources(){
+
+    $.getJSON("../json/images.json?id=1",function(data){
+        var tableObj = $(".gridtable");
+         var tableHead = '    <tr> '+
+         ' <th>名称</th><th>描述</th>'+
+         '<th>操作</th>'+
+         '</tr>';
+         var body = "";
+         $(data).each(
+         function(index,obj){
+         tableObj.text("");
+         body += '<tr>'+
+         '<td >' +
+         obj.name             +
+         '</td>'+
+         '<td >'+
+         obj.description   +
+         '</td>'+
+         '<td><a  href="#openModal" onclick="javaScript:modalEvent(this)" attr="'+
+         obj.addr +
+         '" >进入</a>'+
+         '</td>'+
+         '</tr>' ;
+
+         }
+         );
+         tableObj.append(tableHead+body);
+    })
+}
+
+function modalEvent(obj) {
+    var href = $(obj).attr("attr");
+
+    if(href.indexOf('?') != -1){
+        var add = href.substr(href.indexOf('?')+1,href.length);
+        if( add.indexOf("personal=")>=0 ){
+            var params =prompt("请输入密码","")
+            if( params == '1234567' ){
+                location.href = href;
+            }else{
+                return
+            }
+        }
+    }
+
+    location.href = href;
+
+
+}
